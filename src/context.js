e1b7b19883854ad70a01b7cf60e1b41b91207537
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-07 12:27:43 +0000

    chore: maintenance

--- src/context.js
+++ src/context.js
@@ -7,16 +7,16 @@ import {
   SET_LOADING,
   CHANGE_NEXT_PAGE,
   CHANGE_PREV_PAGE,
-  REMOVE_STORY
+  REMOVE_STORY,
 } from "./actions";
 
 const AppContext = createContext({});
 const initialState = {
-  query: "REACT",
+  query: "",
   isLoading: true,
   hits: [],
-  nbHits:0,
-  nbPages:0,
+  nbHits: 0,
+  nbPages: 0,
   page: 0,
   pageNo: 0,
 };
@@ -29,7 +29,7 @@ const AppProvider = ({ children }) => {
   const setQuery = (query) => {
     dispatch({ type: SET_QUERY, payload: query });
   };
-  
+
   const nextPage = () => {
     if (state.pageNo === state.nbPages - 1) {
       dispatch({ type: CHANGE_NEXT_PAGE, payload: 0 });
@@ -46,26 +46,33 @@ const AppProvider = ({ children }) => {
     dispatch({ type: CHANGE_PREV_PAGE, payload: state.pageNo - 1 });
   };
 
-  const removeStory = (id)=>{
-    const removed = state.hits.filter(({objectID})=>{
-      return objectID !== id
-    })
-    dispatch({type: REMOVE_STORY, payload : removed})
-  }
+  const removeStory = (id) => {
+    const removed = state.hits.filter(({ objectID }) => {
+      return objectID !== id;
+    });
+    dispatch({ type: REMOVE_STORY, payload: removed });
+  };
+
   const fetchData = async () => {
     dispatch({ type: SET_LOADING, payload: true });
     const response = await fetch(
-      `${API_ENDPOINT}/search?query=${state.query}&page=${state.pageNo}`
+      `${API_ENDPOINT}/search?query=${
+        !state.query ? "React" : state.query
+      }&page=${state.pageNo}`
     );
+
     const { hits, nbHits, nbPages, page } = await response.json();
     dispatch({ type: SET_LOADING, payload: false });
     dispatch({ type: SET_DATA, payload: { hits, nbHits, nbPages, page } });
   };
+
   // debounced useEffect
   useDebounce(fetchData, 300, state.query, state.pageNo);
 
   return (
-    <AppContext.Provider value={{ ...state, setQuery, nextPage, prevPage, removeStory }}>
+    <AppContext.Provider
+      value={{ ...state, setQuery, nextPage, prevPage, removeStory }}
+    >
       {children}
     </AppContext.Provider>
   );
